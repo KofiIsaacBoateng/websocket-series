cc26431fafd3e53d21fc3f197d51f38bdc82ef94
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-11-09 20:03:24 +0000

    update: chat

--- 01-basic-text-messaging-app/frontend/src/pages/chat/Chat.jsx
+++ 01-basic-text-messaging-app/frontend/src/pages/chat/Chat.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/chats.styles.css";
+import Sidebar from "./components/Sidebar";
+import ChatList from "./components/ChatList";
+
+// temporaries
+import Lottie from "react-lottie";
+import animationData from "../../assets/lottie/lottie-04.json";
 
 function Chat() {
-  return <div>Chat</div>;
+  const defaultLottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  };
+  return (
+    <div className="main">
+      {/**** sidebar */}
+      <Sidebar />
+      {/**** chats display */}
+      <ChatList />
+      {/**** messages huge box */}
+      <div className="conversation">
+        <div className="conversation-messages">
+          <div className="conversation-messages-lottie">
+            <Lottie style={{ height: "50%" }} options={defaultLottieOptions} />
+            <h2 className="conversation-messages-lottie-message">
+              Select a chat to start a conversation.
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
