d623dcc0545e2b61508247b82df5166ef5847651
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-12-05 20:01:29 +0000

    custom image update

--- 01-basic-text-messaging-app/frontend/src/components/Image.jsx
+++ 01-basic-text-messaging-app/frontend/src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-function Image({ src, style, status, imageStyle }) {
+function Image({ src, style, status, imageStyle, customClassName }) {
   const [loading, setLoading] = useState(false);
   return (
-    <div style={style} className="custom-image">
+    <div style={style} className={`custom-image ${customClassName}`}>
       {status && <div className="custom-image-badge" />}
       <img
         className="custom-image-img"
